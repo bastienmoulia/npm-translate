c2e86a04b393fd91244cc3cd037a0d1b8b80bdba
Bastien <bmoulia@gmail.com>
2016-10-21 17:35:40 +0200

    material design

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AccountsModule } from 'angular2-meteor-accounts-ui';
+import { MdButtonModule } from "@angular2-material/button";
+import { MdToolbarModule } from "@angular2-material/toolbar";
+import { MdInputModule } from "@angular2-material/input";
+import { MdCardModule } from "@angular2-material/card";
+import { MdCoreModule } from "@angular2-material/core";
+import { MdCheckboxModule } from "@angular2-material/checkbox";
+import { MdListModule } from "@angular2-material/list";
 
 import { AppComponent } from './app.component';
 import { PACKS_DECLARATIONS } from './packs';
@@ -33,7 +40,14 @@ import { routes } from './app.routes';
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(routes),
-    AccountsModule
+    AccountsModule,
+    MdCoreModule.forRoot(),
+    MdButtonModule.forRoot(),
+    MdToolbarModule.forRoot(),
+    MdInputModule.forRoot(),
+    MdCardModule.forRoot(),
+    MdCheckboxModule.forRoot(),
+    MdListModule.forRoot()
   ],
   // Main Component
   bootstrap: [ AppComponent ]
